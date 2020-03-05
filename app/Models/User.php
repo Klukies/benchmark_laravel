161da4b79e013ed0fe953e533644fcf78043e39b
Lukas Cornille <lukas.cornille@inthepocket.mobi>
2020-03-05 15:36:38 +0100

    Remove types from non project files

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,17 +12,17 @@ class User extends Authenticatable
     use Notifiable;
 
     /* The attributes that are mass assignable. */
-    protected array $fillable = [
+    protected $fillable = [
         'name', 'email', 'password',
     ];
 
     /* The attributes that should be hidden for arrays. */
-    protected array $hidden = [
+    protected $hidden = [
         'password', 'remember_token',
     ];
 
     /* The attributes that should be cast to native types. */
-    protected array $casts = [
+    protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
