30755345968813f915f297c2c80d0fc0a689c175
Cornille Lukas <lukas.cornille@student.howest.be>
2020-04-08 15:19:28 +0200

    Remove unnecessary code

--- app/Http/Requests/CountryRequest.php
+++ app/Http/Requests/CountryRequest.php
@@ -6,12 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CountryRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return ['countries' => 'required|array'];
     }
